15952d235a50e2bcda43552d47bdd33280bac550
black463 <rd6062818@gmail.com>
2025-07-10 17:13:40 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,21 +1,20 @@
-    // products.js
-    // File ini hanya berisi data produk yang akan diekspor.
+// products.js
+// File ini hanya berisi data produk yang akan diekspor.
 
-    export const products = [
-        { id: 1, name: "Sc Full Buka Olsop", price: 300000, category: "code", image: "https://cdn.lynkid.my.id/products/08-06-2025/1749317735349_5592500.webp" },
-        { id: 2, name: "Home Buka ppob", price: 20000, category: "code", image: "https://cdn.lynkid.my.id/products/08-06-2025/1749317775973_3086705.webp" },
-        { id: 3, name: "Katagori List", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/03-06-2025/1748958192557_6231429.webp" },
-        { id: 4, name: "Toko Shop Modern", price: 50000, category: "code", image: "https://cdn.lynkid.my.id/products/06-06-2025/1749198426560_3810690.webp" },
-        { id: 5, name: "listrik code Token", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/16-06-2025/1750079247826_9954326.webp" },
-        { id: 6, name: "Pulsa All Operator", price: 25000, category: "code", image: "https://cdn.lynkid.my.id/products/25-06-2025/1750853031000_2827900.webp" },
-        { id: 7, name: "Landing Page Ppob", price: 30000, category: "Page", image: "https://cdn.lynkid.my.id/products/28-06-2025/1751049779051_1357367.webp" },
-        { id: 8, name: "Website Blogger", price: 50000, category: "Page", image: "https://cdn.lynkid.my.id/products/06-06-2025/1749219467915_4812292.webp" },
-        { id: 9, name: "Login & Pendaftaran", price: 20000, category: "code", image: "https://cdn.lynkid.my.id/products/12-06-2025/1749713039458_6246827.webp" },
-        { id: 10, name: "Source code poin buka Olshop", price: 20000, category: "code", image: "https://cdn.lynkid.my.id/products/29-06-2025/1751135391057_8740374.webp" },
-        { id: 11, name: "Source code Topup Game", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/30-06-2025/1751230196501_4919393.webp" },
-        { id: 12, name: "Source code Topup E-wallet", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/30-06-2025/1751224687623_8460110.webp" },
-        { id: 13, name: "Source code Hide Produk", price: 15000, category: "code", image: "https://cdn.lynkid.my.id/products/01-07-2025/1751310780790_3370835.webp" },
-        { id: 14, name: "Source code Topup Game Ml", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/01-07-2025/1751312216269_7446272.webp" },
-    ];
-    ```
+export const products = [
+    { id: 1, name: "Sc Full Buka Olsop", price: 300000, category: "code", image: "https://cdn.lynkid.my.id/products/08-06-2025/1749317735349_5592500.webp" },
+    { id: 2, name: "Home Buka ppob", price: 20000, category: "code", image: "https://cdn.lynkid.my.id/products/08-06-2025/1749317775973_3086705.webp" },
+    { id: 3, name: "Katagori List", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/03-06-2025/1748958192557_6231429.webp" },
+    { id: 4, name: "Toko Shop Modern", price: 50000, category: "code", image: "https://cdn.lynkid.my.id/products/06-06-2025/1749198426560_3810690.webp" },
+    { id: 5, name: "listrik code Token", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/16-06-2025/1750079247826_9954326.webp" },
+    { id: 6, name: "Pulsa All Operator", price: 25000, category: "code", image: "https://cdn.lynkid.my.id/products/25-06-2025/1750853031000_2827900.webp" },
+    { id: 7, name: "Landing Page Ppob", price: 30000, category: "Page", image: "https://cdn.lynkid.my.id/products/28-06-2025/1751049779051_1357367.webp" },
+    { id: 8, name: "Website Blogger", price: 50000, category: "Page", image: "https://cdn.lynkid.my.id/products/06-06-2025/1749219467915_4812292.webp" },
+    { id: 9, name: "Login & Pendaftaran", price: 20000, category: "code", image: "https://cdn.lynkid.my.id/products/12-06-2025/1749713039458_6246827.webp" },
+    { id: 10, name: "Source code poin buka Olshop", price: 20000, category: "code", image: "https://cdn.lynkid.my.id/products/29-06-2025/1751135391057_8740374.webp" },
+    { id: 11, name: "Source code Topup Game", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/30-06-2025/1751230196501_4919393.webp" },
+    { id: 12, name: "Source code Topup E-wallet", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/30-06-2025/1751224687623_8460110.webp" },
+    { id: 13, name: "Source code Hide Produk", price: 15000, category: "code", image: "https://cdn.lynkid.my.id/products/01-07-2025/1751310780790_3370835.webp" },
+    { id: 14, name: "Source code Topup Game Ml", price: 30000, category: "code", image: "https://cdn.lynkid.my.id/products/01-07-2025/1751312216269_7446272.webp" },
+];
 
